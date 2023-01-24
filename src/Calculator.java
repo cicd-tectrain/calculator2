4ea43a3f369e377cffbb38878ec5044b5f8124a9
Filip Tisaj <test@test.at>
2023-01-24 14:56:38 +0100

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    //TODO: Macht was!!!
+
+    public int add (int a, int b)
+    {
+        return  a + b;
+    }
+
+    public int substract (int a, int b)
+    {
+        return 0;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(1,2);
+        int sub = calculator.substract(5,3);
+//        int mul = calculator.multiply(2,5);
+//        int div = calculator.divide(6,2);
+
+        System.out.println(sum);
+        System.out.println(sub);
+//        System.out.println(mul);
+//        System.out.println(div);
+    }
+}
